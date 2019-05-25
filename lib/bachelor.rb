7f51597a1021e2c528f6e7bece14432e55891bf0
Ryan Bravo <rrbravo2@gmail.com>
2019-05-25 02:12:44 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,12 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |people|
+    people.each do |k, v|
+      if v == "winner"
+        full_name = people["name"]
+        full_name.split(' ').first
+      end
+    end
+  end    # code here
 end
 
 def get_contestant_name(data, occupation)
