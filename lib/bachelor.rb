08678de4b0286bbd3c08ab3295121f63de258b2a
Ryan Bravo <rrbravo2@gmail.com>
2019-05-25 02:20:40 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,6 @@
 def get_first_name_of_season_winner(data, season)
   data[season.to_sym].each do |people|
-    if people[:status] == "winner"
+    if people[:status] == "Winner"
     return people[:name].split(" ")[0]
     end
   end    # code here
